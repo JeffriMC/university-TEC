525e37cc6d14e8209ac56017f12b5847ac3c70dc
Jeffri <jeffri@jeffri>
2019-10-10 17:02:01 -0600

    commit de marco hecho en la pc de jeffri

--- caso#10_IOT/src/aplication/GLOBALS.java
+++
@@ -1,9 +0,0 @@
-package aplication;
-
-import models.Splay_Tree;
-import models.Tree_AyA;
-
-public interface GLOBALS {
-	public static Tree_AyA AYA_DB = new Tree_AyA();
-	public static Splay_Tree AYA_SPLAY = new Splay_Tree();
-}
